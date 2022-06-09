20d60a4ae757d0e369d2aa2c6280a2801c9ef853
J2coding <kimmyjmueni@gmail.com>
2022-06-09 13:19:18 +0300

    file

---
+++ tic-tac-toe.py
@@ -0,0 +1,17 @@
+"""
+Created by: Josephine Mueni 
+Date: 9/6/2022
+"""
+import random
+#A tic tac toe game in which two players seek in
+# alternate turns to complete a row,column or
+#a diagonal with either three x's or three o's 
+# drawn in the spaces of a grid of nine squares.
+def main(board):
+	print('Welcome to Tic Tac Toe game')
+	print(' | | ')
+	print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+	print('--------------')
+	
+
+	
